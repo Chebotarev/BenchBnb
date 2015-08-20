1f4041aa962fee95b50e541067787e764c53f36f
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-08-20 12:45:37 -0700

    Create Api routes for benches, make seeds

--- app/controllers/api/benches_controller.rb
+++ app/controllers/api/benches_controller.rb
@@ -1,2 +1,20 @@
 class Api::BenchesController < ApplicationController
+
+  def index
+    @benches = Bench.all
+
+    render json: @benches
+  end
+
+  def create
+    @bench = Bench.new(bench_params)
+
+    render json: @bench
+  end
+
+  private
+
+  def bench_params
+    params.require(:bench).permit(:description, :lat, :lng)
+  end
 end
